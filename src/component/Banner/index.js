e39e143063166af1eea2bf315c753256df1d9600
Vinicius Santana <Vinicius.Lima33@Outlook.com>
2023-08-16 18:26:47 -0300

    Início do Desenvolvimento utilizando o React e o figma.

---
+++ src/component/Banner/index.js
@@ -0,0 +1,13 @@
+import './banner.css'
+
+function Banner() {
+    // JSX
+    return(
+        // Usar className!
+        <header className="banner">
+            <img src="/img/banner.png" alt="Banner principal da página do organo"/>
+        </header>
+    )
+}
+
+export default Banner;
---
+++ src/component/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+function CampoTexto() {
+    return(
+        <div className="campo__texto">
+            <label>Nome</label>
+            <input placeholder='Digite o seu nome' />
+        </div>
+    )
+}
+
+export default CampoTexto;
