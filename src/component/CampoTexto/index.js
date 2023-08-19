29e054b19b2a99b71406cc3f86262f9efee16f76
Vinicius Santana <Vinicius.Lima33@Outlook.com>
2023-08-18 22:29:30 -0300

    Controlando o estado.

--- src/component/CampoTexto/index.js
+++ src/component/CampoTexto/index.js
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 function CampoTexto(props) {
+
+    // let valor = ''
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return(
         <div className="campo__texto">
             <label>{props.label}</label>
-            <input required={props.required} placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitado} required={props.required} placeholder={props.placeholder} />
         </div>
     )
 }
--- src/component/Formulário/index.js
+++ src/component/Formulário/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
@@ -15,18 +16,23 @@ function Formulario() {
 
     ]
 
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Card cadastrado com sucesso!')
+        console.log('Card cadastrado com sucesso!', nome, cargo, imagem)
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do time!</h2>
-                <CampoTexto required={true} label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto required={true} label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <CampoTexto required={true} label="Nome" placeholder="Digite seu nome" valor={nome} aoAlterado={valor => setNome(valor)} />
+                <CampoTexto required={true} label="Cargo" placeholder="Digite seu cargo" valor={cargo} aoAlterado={valor=>setCargo(valor)} />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" valor={imagem} aoAlterado={valor=>setImagem(valor)} />
                 <ListaSuspensa label="Times" itens={times} />
                 <Botao texto="Criar Card"/>
             </form>
